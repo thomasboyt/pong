fc381c47886b2d3486b3253c6a865dfbc5d28e41
Thomas Boyt <me@thomasboyt.com>
2018-08-03 20:06:17 -0400

    Hook up networking components

--- src/components/Game.ts
+++ src/components/Game.ts
@@ -1,8 +1,40 @@
 import { Component } from 'pearl';
 import { Tag, ZIndex } from '../types';
+import NetworkingClient from '../networking/components/NetworkingClient';
+import NetworkingHost from '../networking/components/NetworkingHost';
 
-export default class Game extends Component<null> {
-  init() {
-    console.log('hello world');
+interface Opts {
+  isHost: boolean;
+  roomCode?: string;
+}
+
+const groovejetUrl = 'localhost:3000';
+
+export default class Game extends Component<Opts> {
+  init(opts: Opts) {
+    if (opts.isHost) {
+      this.runCoroutine(this.initializeHost());
+    } else {
+      this.runCoroutine(this.initializeClient(opts.roomCode!));
+    }
+  }
+
+  *initializeHost() {
+    const roomCode = yield this.getComponent(NetworkingHost).connect(
+      groovejetUrl
+    );
+    this.handleRoomCode(roomCode);
+  }
+
+  *initializeClient(roomCode: string) {
+    yield this.getComponent(NetworkingClient).connect({
+      groovejetUrl,
+      roomCode,
+    });
+    this.handleRoomCode(roomCode);
+  }
+
+  handleRoomCode(roomCode: string) {
+    console.log('roomCode:', roomCode);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { createPearl } from 'pearl';
 import Game from './components/Game';
+import NetworkingHost from './networking/components/NetworkingHost';
+import NetworkingClient from './networking/components/NetworkingClient';
+import networkedPrefabs from './networkedPrefabs';
 
-async function main() {
+interface Opts {
+  isHost: boolean;
+  roomCode?: string;
+}
+
+async function main(opts: Opts) {
+  const { isHost, roomCode } = opts;
+
+  let networkingComponent;
+
+  if (isHost) {
+    networkingComponent = new NetworkingHost({
+      prefabs: networkedPrefabs,
+    });
+  } else {
+    networkingComponent = new NetworkingClient({
+      prefabs: networkedPrefabs,
+    });
+  }
+
   const pearl = await createPearl({
-    rootComponents: [new Game()],
+    rootComponents: [new Game({ isHost, roomCode }), networkingComponent],
     width: 320,
     height: 240,
     backgroundColor: 'black',
@@ -13,4 +35,10 @@ async function main() {
   pearl.renderer.scale(2);
 }
 
-main();
+const params = new URLSearchParams(document.location.search.slice(1));
+const roomCode = params.get('roomCode');
+if (roomCode !== null) {
+  main({ isHost: false, roomCode });
+} else {
+  main({ isHost: true });
+}
