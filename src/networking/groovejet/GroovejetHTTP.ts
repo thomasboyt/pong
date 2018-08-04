3a668a7c36cee784b25c9a0b2eb82c2905a9d305
Thomas Boyt <me@thomasboyt.com>
2018-08-04 01:21:50 -0400

    Use fetch() instead of axios for groovejet HTTP
    no need for an extra dependency

--- src/networking/groovejet/GroovejetHTTP.ts
+++ src/networking/groovejet/GroovejetHTTP.ts
@@ -1,7 +1,13 @@
-import axios from 'axios';
-
 export async function createRoom(lobbyServer: string): Promise<string> {
-  const resp = await axios.post(`//${lobbyServer}/rooms`);
-  const code = resp.data.code;
+  const resp = await fetch(`//${lobbyServer}/rooms`, {
+    method: 'POST',
+  });
+
+  // TODO: resp.ok may not exist on iPhone/Android browsers?
+  if (!resp.ok) {
+    throw new Error(`error creating room: ${resp.status} ${resp.statusText}`);
+  }
+
+  const { code } = await resp.json();
   return code;
 }
